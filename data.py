797aa17678859e7bcfd728398cd504dd54557907
myname <you@example.com>
2020-07-13 12:11:16 -0400

    first

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body' : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+            'author' : 'Kristy TwoFish',
+            'create_date': '13.07.20'
+        },
+
+                {
+            'id':2,
+            'title': 'Article Two',
+            'body' : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+            'author' : 'Kristy Fish',
+            'create_date': '13.07.20'
+        },
+
+                {
+            'id':3,
+            'title': 'Article Tree',
+            'body' : 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+            'author' : 'Kristy Fishy',
+            'create_date': '13.07.20'
+        }
+    ]
+    return articles
