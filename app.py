092ba606d011cd73667c533f061748d64d2306df
myname <you@example.com>
2020-07-13 13:46:21 -0400

    user registration

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
-#from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
+
+
+#config mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Passtwofish'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 Articles = Articles()
 
@@ -40,9 +50,26 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        pass
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #create cursor (cursor- allows to interact to DB execute the statement and return it back)
+        cur = mysql.connection.cursor()
+        #execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        #connect to DB
+        mysql.connection.commit()
+        #close connection
+        cur.close()
+        flash('You are registered', 'success')
+
+        redirect(url_for('home'))
     return render_template('register.html', form=form)
 
 
+
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug= True)
