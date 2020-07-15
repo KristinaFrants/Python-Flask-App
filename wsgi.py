16e96592e09c8ac34373b481414b10dcc9a5e247
myname <cristina.chiticari@gmail.com>
2020-07-15 14:09:50 -0400

    uncoment

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-# from app import app as application
+from app import app as application
 
-# app = application()
+app = application()
 
-# if __name__ == "__main__":
-#     application.run()
+if __name__ == "__main__":
+    application.run()
